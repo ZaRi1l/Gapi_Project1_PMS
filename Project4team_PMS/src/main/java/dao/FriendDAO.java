acee0a510fcc052382ee795dddbd71adfccb3536
ZaRi1l <zari_09@naver.com>
2025-02-04 11:36:40 +0900

    동료 추가 구현, 이메일 초대 구현

--- Project4team_PMS/src/main/java/dao/FriendDAO.java
+++ Project4team_PMS/src/main/java/dao/FriendDAO.java
@@ -1,5 +1,6 @@
 package dao;
 
+import java.net.UnknownHostException;
 import java.sql.*;
 import javamailApi.*;
 
@@ -14,14 +15,20 @@ public class FriendDAO {
 	private ResultSet rs;
 
 	// 동료 추가 및 이메일 전송
-	public String addFriend(String customerId, String email) {
+	public String addFriend(String customerId, String dashboardId, String email) throws UnknownHostException {
 		String queryCheckUser = "SELECT CUSTOMER_ID, JSONSTR FROM CLIENT WHERE CUSTOMER_ID = ?";
-		String queryAddFriend = "INSERT INTO FRIENDLIST (CUSTOMER_ID, FRIEND_ID) VALUES (?, ?)";
+//		String queryAddFriend = "INSERT INTO FRIENDLIST (CUSTOMER_ID, FRIEND_ID) VALUES (?, ?)";
+		String queryAddClient_dashboard = "INSERT INTO CLIENT_DASHBOARD (CUSTOMER_ID, DASHBOARD_ID) VALUES (?, ?)";
+		
+		String queryAddInvite = "INSERT INTO INVITE (FRIEND_ID, DASHBOARD_ID) VALUES (?, ?)";
 
 		try {
 			connDB();
+
+			System.out.println("보내는 사람: " + customerId + " 받는 사람: " + email + " 초대하는 대시보드: " + dashboardId);
 			
-			System.out.println("보내는 사람: " + customerId + " 받는 사람: " + email);
+			// 0. 이메일로 이미 대시보드에 있는 사용자인지 확인
+			
 
 			// 1. 이메일로 사용자가 존재하는지 확인
 			stmt = con.prepareStatement(queryCheckUser);
@@ -32,9 +39,14 @@ public class FriendDAO {
 				String userName = rs.getString("CUSTOMER_ID");
 
 				// 2. 사용자 존재 시 동료 목록에 추가
-				stmt = con.prepareStatement(queryAddFriend);
-				stmt.setString(1, customerId);
-				stmt.setString(2, email);
+//				stmt = con.prepareStatement(queryAddFriend);
+//				stmt.setString(1, customerId);
+//				stmt.setString(2, email);
+//				int result = stmt.executeUpdate();
+
+				stmt = con.prepareStatement(queryAddClient_dashboard);
+				stmt.setString(1, email);
+				stmt.setString(2, dashboardId);
 				int result = stmt.executeUpdate();
 
 				if (result > 0) {
@@ -45,7 +57,19 @@ public class FriendDAO {
 					return "동료 추가 실패";
 				}
 			} else {
-				return "해당 이메일에 해당하는 사용자가 없습니다.";
+				// 초대 메일에 추가 (나중에 회원가입할때 불러올 것임)
+				stmt = con.prepareStatement(queryAddInvite);
+				stmt.setString(1, email);
+				stmt.setString(2, dashboardId);
+				int result = stmt.executeUpdate();
+				
+				if (result > 0) {
+					// 이메일 전송
+					EmailSender.sendInvitationEmail2(customerId, email, customerId);
+					return "OK";
+				} else {
+					return "동료 추가 실패";
+				}
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
