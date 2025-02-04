e244878ba8d2e7209677991d3fd098570e3c5851
ZaRi1l <zari_09@naver.com>
2025-02-04 11:42:37 +0900

    대시보드에 있던 사용자면 초대 불가능하게 바꿈

--- Project4team_PMS/src/main/java/dao/FriendDAO.java
+++ Project4team_PMS/src/main/java/dao/FriendDAO.java
@@ -16,19 +16,26 @@ public class FriendDAO {
 
 	// 동료 추가 및 이메일 전송
 	public String addFriend(String customerId, String dashboardId, String email) throws UnknownHostException {
+		String queryCheckDashboard = "SELECT CUSTOMER_ID, DASHBOARD_ID FROM CLIENT_DASHBOARD WHERE CUSTOMER_ID = ? AND DASHBOARD_ID = ?";
 		String queryCheckUser = "SELECT CUSTOMER_ID, JSONSTR FROM CLIENT WHERE CUSTOMER_ID = ?";
 //		String queryAddFriend = "INSERT INTO FRIENDLIST (CUSTOMER_ID, FRIEND_ID) VALUES (?, ?)";
 		String queryAddClient_dashboard = "INSERT INTO CLIENT_DASHBOARD (CUSTOMER_ID, DASHBOARD_ID) VALUES (?, ?)";
-		
+
 		String queryAddInvite = "INSERT INTO INVITE (FRIEND_ID, DASHBOARD_ID) VALUES (?, ?)";
 
 		try {
 			connDB();
 
 			System.out.println("보내는 사람: " + customerId + " 받는 사람: " + email + " 초대하는 대시보드: " + dashboardId);
-			
+
 			// 0. 이메일로 이미 대시보드에 있는 사용자인지 확인
-			
+			stmt = con.prepareStatement(queryCheckDashboard);
+			stmt.setString(1, email);
+			stmt.setString(2, dashboardId);
+			rs = stmt.executeQuery();
+			if (rs.next()) {
+				return "이미 대시보드에 존재하는 사용자입니다";
+			}
 
 			// 1. 이메일로 사용자가 존재하는지 확인
 			stmt = con.prepareStatement(queryCheckUser);
@@ -62,7 +69,7 @@ public class FriendDAO {
 				stmt.setString(1, email);
 				stmt.setString(2, dashboardId);
 				int result = stmt.executeUpdate();
-				
+
 				if (result > 0) {
 					// 이메일 전송
 					EmailSender.sendInvitationEmail2(customerId, email, customerId);
